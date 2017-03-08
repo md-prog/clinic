ee060f10c5a383b03f3d42d0b3ed6beff02a4a62
ifield <ifield@STORMPETREL>
2017-03-07 22:18:33 -0700

    vue.js webpack loader fix for case keyword

--- src/ClinicaCloudPlatform_GUI/VueApp/components/list/WorklistCaseDetail.vue
+++ src/ClinicaCloudPlatform_GUI/VueApp/components/list/WorklistCaseDetail.vue
@@ -1,32 +1,9 @@
 ﻿<template>
     <div class="container-fluid pl-0">
         <div class="card card-accent-primary card-compact">
-            <div class="card-header card-compact">{{case.id}} - {{case.type}}</div>
+            <div class="card-header card-compact"><span class="caseLabel">Case</span> # {{_case.id}} - {{_case.type}}</div>
             <div class="card-block card-compact">
-                <div v-for="row in organization.customData.caseAccessionSections.rows" class="row m-0">
-                    <!--v-bind:class="col.class"-->
-                    <div v-for="col in row.cols" v-bind:class="col.class">
-                        <div class="card card-compact card-no-outer-border">
-                            <div class="card-header card-compact">{{col.sectionName}}</div>
-                            <div class="card-block card-compact wrap-unset">
-                                <div class="row m-0" v-for="att in getcaseAttributesBySection(col.sectionName, case.type)">
-                                    <div class="col-sm-auto mr-1"><label v-bind:for="'attValue' + case.id + att.name" class="label-black"><strong>{{att.name}}</strong>:</label></div>
-                                    <div class="col-sm-auto" v-bind:id="'attValue' + case.id + att.name">
-                                        <div v-if="Array.isArray(currentcaseAttributeValue(case, att.name, false))" class="row m-0 p-0">
-                                            <div v-for="value in currentcaseAttributeValue(case, att.name, false)" class="col-sm-12 m-0 p-0">
-                                                {{value.name}}
-                                            </div>
-                                        </div>
-                                        <div v-else class="m-0 p-0">
-                                            {{currentcaseAttributeValue(case, att.name, false).name}}
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
+                
             </div>
         </div>
     </div>
@@ -44,7 +21,7 @@
         name: "WorklistCaseDetail",
         props:{
             organization: Object,
-            case: Object
+            _case: Object
         },
         components: {},
         mixins: [customDataHelpersMixin],
